41995754d35f398f1ea4e0559e58001f2be83ea0
Aurelien1997 <tawpo36@gmail.com>
2022-04-28 19:36:00 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+const form = document.getElementById('form');
+
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  const searchInput = document.getElementById("searchInput").value;
+  console.log(searchInput);
+  getMovies(searchInput);
+})
+
+const getMovies = async (i) => {
+fetch(`https://www.omdbapi.com/?s=${i}&plot=full&apikey=69d24a2a`).then((data) =>{
+  return data.json();
+}).then((data)=>{
+  console.log(data);
+  let data1 = "";
+  data.Search.map((values)=>{
+    data1 = `
+    <div class="movies">
+      <div id="movieApi" class="movie card flex-row">
+        <img class="card-img-left" src="${values.Poster}"/>
+        <div class="card-body">
+          <div class="header"><p>${values.Title}</p></div>
+          <div class="body"></div>
+        </div>
+      </div>
+    </div>`
+    document.getElementById("containerApi").innerHTML += data1;
+  });
+}).catch((err)=>{
+    console.log(err)
+})
+}
+
