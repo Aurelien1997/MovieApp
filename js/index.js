84fed0e895ae694dd36b31020546d5b3b0775f99
Aurelien1997 <tawpo36@gmail.com>
2022-04-29 11:03:41 +0100

    js change

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 const form = document.getElementById('form');
-
+const apikey = "69d24a2a";
+let test1;
 
 form.addEventListener('submit', (e) =>{
   e.preventDefault()
@@ -10,26 +11,73 @@ form.addEventListener('submit', (e) =>{
 })
 
 const getMovies = async (i) => {
-fetch(`https://www.omdbapi.com/?s=${i}&plot=full&apikey=69d24a2a`).then((data) =>{
-  return data.json();
-}).then((data)=>{
-  console.log(data);
-  let data1 = "";
-  data.Search.map((values)=>{
-    data1 = `
-    <div class="movies p-3">
-      <div id="movieApi" class="movie card flex-row">
-        <img class="card-img-left" src="${values.Poster}"/>
-        <div class="card-body">
-          <div class="header"><p>${values.Title}</p></div>
-          <div class="body"></div>
+  fetch(`https://www.omdbapi.com/?s=${i}&plot=full&apikey=${apikey}`).then((data) =>{
+    return data.json();
+  }).then((data)=>{
+    console.log(data);
+    let data1 = "";
+    data.Search.map((values)=>{
+      data1 = `
+      <div class="movies p-3">
+        <div id="movieApi" class="movie card flex-row">
+          <img class="card-img-left" src="${values.Poster}"/>
+          <div class="card-body">
+            <div class="header">
+              <h1>${values.Title}</h1>
+            </div>
+            <div class="body">
+              <p class="text-info">${values.Year}</p>
+              <button class="test1 btn btn-primary" id=${values.imdbID}>info</button>
+              <p>${values.imdbID}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>`
-    document.getElementById("containerApi").innerHTML += data1;
-  });
-}).catch((err)=>{
-    console.log(err)
-})
+      </div>`
+      details(values.imdbID);
+      document.getElementById("containerApi").innerHTML += data1;
+
+          });
+          test1 = document.querySelectorAll('.test1');
+  }).catch((err)=>{
+      console.log(err)
+  })
 }
 
+window.addEventListener('click', (e) => {
+  if(e.target.classList.contains("test1")){
+    details(e.target.id);
+  }
+})
+
+const details = (id) => {
+  fetch(`https://www.omdbapi.com/?i=${id}&apikey=${apikey}&plot=full`)
+  .then((data) => {
+    return data.json();
+  })
+  .then((movie) => {
+    console.log(movie);
+    let data2 = "";
+    data2 = `
+    <div class="movies p-3">
+      <div id="movieApi" class="movie card flex-row">
+        <img class="card-img-left" src="${movie.Poster}"/>
+        <div class="card-body">
+          <div class="header">
+            <h1>${movie.Title}</h1>
+            <p class="text-info">${movie.Released}</p>
+            
+          </div>
+          <div class="body">
+          <p class="text-success">${movie.Plot}</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    document.getElementById("test").innerHTML = data2;
+  }).catch((err)=>{
+      console.log(err)
+  })
+}
+/*
+details(movieId);
+details("tt0096895");*/
